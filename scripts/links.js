674615ccbfd2edeab8e3f37b1427b3cfb48ccb01
allredty <all21059@byui.edu>
2024-06-27 10:43:55 -0600

    Got the links working finally.

--- scripts/links.js
+++ scripts/links.js
@@ -4,36 +4,47 @@ const linksURL = "https://allredty.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.weeks);
 }
-
-
+  
 getLinks();
+  
 
 function displayLinks(weeks) {
-    const linksContainer = document.querySelector('#the-weeks');
-    weeks.forEach(week => {
-        const weekSection = document.createElement('div');
-        weekSection.classList.add('week-section');
+  const container = document.querySelector('#activity-links');
 
-        const weekTitle = document.createElement('h2');
-        weekTitle.textContent = week.week;
-        weekSection.appendChild(weekTitle);
+  weeks.forEach(week => {
+    // Create the week heading
+    const weekHeader = document.createElement('h2');
+    weekHeader.textContent = week.week;
+    container.appendChild(weekHeader);
 
-        const linksList = document.createElement('ul');
-        week.links.forEach(link => {
-            const listItem = document.createElement('li');
-            const anchor = document.createElement('a');
-            anchor.href = link.url;
-            anchor.textContent = link.title;
-            listItem.appendChild(anchor);
-            linksList.appendChild(listItem);
-        });
+    // Create the list of activities
+    const ul = document.createElement('ul');
 
-        weekSection.appendChild(linksList);
-        linksContainer.appendChild(weekSection);
+    week.links.forEach((link, index) => {
+      const li = document.createElement('li');
+      li.style.display = "inline"; // Display list items inline
+
+      const a = document.createElement('a');
+      a.href = `${baseURL}${link.url}`;
+      a.textContent = link.title;
+      li.appendChild(a);
+
+      ul.appendChild(li);
+
+      // Add a pipe separator except for the last link
+      if (index < week.links.length - 1) {
+        const separator = document.createTextNode(" | ");
+        ul.appendChild(separator);
+      }
     });
+
+    container.appendChild(ul);
+  });
 }
+
+  
 
 
 
